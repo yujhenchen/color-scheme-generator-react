ca6a7397b386f8435b894902ea467bbc0ce10653
yujhenchen <yujhen.chen93@gmail.com>
2023-12-25 16:32:26 +0800

    fix card style

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 
 # Vertical align text in <p>?
+flex items-center justify-center
 
 # How to display check mark in the selected <option> in <select>?
 
@@ -11,9 +12,10 @@ set self-stretch to the flex item
 # Scale to increase the size of flex item without changing the layout of the parent?
 
 # click copy to clipboard
+navigator.clipboard.writeText(color);
 
 # Show copy toast and hide after seconds
-
+debounce
 
 # debounce does not work?
 Root cause:
--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -4,10 +4,10 @@ export default function ColorCard({ colorName, colorValue, onClick }) {
   return (
     <div
       onClick={() => onClick(colorValue)}
-      className="relative grow hover:scale-105 hover:z-10 duration-200"
+      className="relative grow hover:scale-105 hover:z-10 hover:shadow-lg duration-200"
     >
       <div style={{ backgroundColor: colorValue }} className="h-full"></div>
-      <p className="h-12 absolute bottom-0 left-0 bg-white dark:bg-[#1F2937] text-black dark:text-white w-full text-center text-xs">
+      <p className="h-12 absolute bottom-0 left-0 bg-white dark:bg-[#1F2937] text-black dark:text-white w-full text-xs flex items-center justify-center">
         {colorName}
       </p>
     </div>

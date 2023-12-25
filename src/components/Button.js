50628d6a608456b90fbdf8611b8fbf5173cbde3b
yujhenchen <yujhen.chen93@gmail.com>
2023-12-25 14:26:27 +0800

    add button style, add dropdown style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Button({ text, onClick }) {
   return (
-    <button className="text-xs p-2" onClick={onClick}>
+    <button
+      className="text-xs p-2 border-solid border-2 border-[#9CA3AF] rounded"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function DropDown({ options, onSelect, selectedOption }) {
   return (
     <select
-      className="w-1/3 p-2 text-base"
+      className="w-1/3 p-2 text-base border-solid border-2 border-[#9CA3AF] rounded"
       onChange={(event) => onSelect(event.target.value)}
       value={selectedOption}
     >
